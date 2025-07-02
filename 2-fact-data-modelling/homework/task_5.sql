d35da4b607dff036f4de8e94817a9579ec9abbc6
Marco Di Spirito <marco.n.dispirito@gmail.com>
2025-07-02 10:19:26 -0700

    hosts cumulated table homework

--- 2-fact-data-modelling/homework/task_5.sql
+++ 2-fact-data-modelling/homework/task_5.sql
@@ -2,3 +2,10 @@
 - A DDL for `hosts_cumulated` table 
   - a `host_activity_datelist` which logs to see which dates each host is experiencing any activity
 */
+
+CREATE TABLE hosts_cumulated (
+	host TEXT,
+	dates_active DATE[],
+	date DATE,
+	PRIMARY KEY (host, date)
+)
--- 2-fact-data-modelling/homework/task_6.sql
+++ 2-fact-data-modelling/homework/task_6.sql
@@ -1,1 +1,33 @@
 -- The incremental query to generate `host_activity_datelist`
+
+/*
+This query is just for learning/lab purposes.
+In a production setting, we'd pass the dates as params to do a daily incremental load.
+*/
+INSERT INTO hosts_cumulated
+WITH yesterday AS (
+	SELECT *
+	FROM hosts_cumulated
+	WHERE date = DATE('2022-12-31')
+), today AS (
+	SELECT
+		host,
+		DATE(CAST(event_time AS TIMESTAMP)) AS date_active
+	FROM events
+	WHERE DATE(CAST(event_time AS TIMESTAMP)) = DATE('2023-01-01')
+	AND host IS NOT NULL
+	GROUP BY host, DATE(CAST(event_time AS TIMESTAMP))
+)
+SELECT
+	COALESCE(t.host, y.host) AS host,
+	CASE
+		WHEN y.dates_active IS NULL
+			THEN ARRAY[t.date_active]
+		WHEN t.date_active IS NULL
+			THEN y.dates_active
+		ELSE ARRAY[t.date_active] || y.dates_active
+	END AS dates_active,
+	COALESCE(t.date_active, y.date + INTERVAL '1 day') AS date
+FROM today t
+FULL OUTER JOIN yesterday y
+ON t.host = y.host
